93f589807a9ab8b4600319e05695f85a824630b3
Terrac Skiens <tskiens@apple.com>
2017-12-06 16:01:45 -0800

    initial fork

---
+++ sonarr.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+SERVICE="sonarr"
+IMAGE="linuxserver/sonarr"
+VERSION="latest"
+MY_IP=$(host home.terrac.com | awk '{print $NF}')
+LOCALDIR="/data01/services/${SERVICE}"
+DOWNLOADS="/data01/incoming/tv"
+COMPLETE="/data01/complete/tv"
+
+test  -d ${LOCALDIR}  || mkdir -p ${LOCALDIR}
+
+docker stop ${SERVICE}
+docker rm ${SERVICE}
+
+
+sudo docker run -d \
+  --restart=always \
+  --name=${SERVICE} \
+  --hostname=${HOSTNAME} \
+  -v /etc/localtime:/etc/localtime:ro \
+  -e PUID=1001 -e PGID=1001 \
+  -p 8989:8989 \
+  -v ${LOCALDIR}:/config \
+  -v ${DOWNLOADS}:/downloads \
+  -v ${COMPLETE}:/tv \
+  ${IMAGE}:${VERSION}
+
+docker logs ${SERVICE}
